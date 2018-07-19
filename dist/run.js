6a03bcb1b701a2d95a71bab8e9b6db8c576524be
Arjun Patrawala <ajpat1234@gmail.com>
2018-07-19 15:07:26 -0700

    small

--- dist/run.js
+++ dist/run.js
@@ -1,6 +1,6 @@
 let inputNodeNumber = 3;
 let net = nn.build({
-    nodeNums: [inputNodeNumber, 12, 18],
+    nodeNums: [inputNodeNumber, 12, 122, 18],
     activationFunction: nn.activation.sigmoid(),
     randomizeWeights: {
         from: -1,
@@ -24,7 +24,7 @@ console.timeEnd();
 
 let output = [0, .6, .2222, 0, .6666, .2, 0, .6, .8, .694, .6, .2, 0, .6, .2, 0, .6, .2];
 console.time();
-for (let i = 0; i < 40000; i++)
+for (let i = 0; i < 4000; i++)
     net.train([2, 4, 1], output);
 console.timeEnd();
 
